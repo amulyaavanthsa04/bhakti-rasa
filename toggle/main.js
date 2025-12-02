9d6b5fc3db32a2247b54fa2a25ac92b3acf79569
amulyaavanthsa04 <amulavanthsa.04@gmail.com>
2025-12-02 08:42:54 -0500

    Add files via upload

---
+++ toggle/main.js
@@ -0,0 +1,11 @@
+
+let button = document.getElementById("swap-button");
+let container = document.getElementById("container");
+
+button.addEventListener("click",function(){
+ 
+    container.classList.toggle("watered");
+
+
+});
+
